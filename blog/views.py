949fe7e609eef07f4892a12edde54d5f7538d8b5
hrosser <hrosser@unomaha.edu>
2018-03-31 18:03:29 -0500

    Watson Blog Commit

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,54 @@
-from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post, Comment
-from django.shortcuts import render, get_object_or_404
 from .forms import PostForm, CommentForm
+from django.shortcuts import redirect
+import json
+from watson_developer_cloud import ToneAnalyzerV3
+from watson_developer_cloud import LanguageTranslatorV2 as LanguageTranslator
 
-# Create your views here.
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    tone_analyzer = ToneAnalyzerV3(
+        version='2016-05-19',
+        username='2ea4fcb2-cf0a-4788-8c42-e6e5b13d1ccf',
+        password='DfDiRNdrUxj5')
+
+    language_translator = LanguageTranslator(
+        username='4e83639a-1b52-446d-894c-f67c8853352d',
+        password='6gDIbpcVi4yt')
+
+    # print(json.dumps(translation, indent=2, ensure_ascii=False))
+
+    for post in posts:
+        posting = post.text
+        toneObj = json.dumps(tone_analyzer.tone(tone_input=posting,
+                                   content_type="text/plain"), indent=2)
+        post.toneObj2 = json.loads(toneObj)
+        post.angerScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][0]['score']
+        post.disgustScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][1]['score']
+        post.fearScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][2]['score']
+        post.joyScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][3]['score']
+        post.sadScore = post.toneObj2['document_tone']['tone_categories'][0]['tones'][4]['score']
+
+        translation = language_translator.translate(
+            text=post.text,
+            source='en',
+            target='es')
+        obj= json.dumps(translation, indent=2, ensure_ascii=False)
+        post.obj2 = json.loads(obj)
+
+
+
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    # Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -26,6 +63,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -39,7 +77,6 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
-
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -53,13 +90,13 @@ def add_comment_to_post(request, pk):
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
-@login_required
+
 def comment_approve(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
     return redirect('post_detail', pk=comment.post.pk)
 
-@login_required
+
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
